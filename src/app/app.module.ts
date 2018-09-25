a09e9d51588c4c211248f84112dd995e37f50f70
Luis Felipe <felipe096@gmail.com>
2018-09-25 13:24:32 -0500

    Added basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { routing } from './routes';
+import { ProductTreeComponent } from './products/containers/product-tree/product-tree.component';
+import { CartComponent } from './products/containers/cart/cart.component';
+import { HeaderComponent } from './products/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FilterFormComponent } from './products/components/filter-form/filter-form.component';
+import { TreeComponent } from './products/components/tree/tree.component';
+
+import {
+  MatSidenavModule,
+  MatIconModule,
+  MatNavList,
+  MatToolbarModule } from '@angular/material/';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductTreeComponent,
+    CartComponent,
+    HeaderComponent,
+    MatNavList,
+    FilterFormComponent,
+    TreeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
